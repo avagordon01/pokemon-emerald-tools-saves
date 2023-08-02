3a965d88f8e3cdec36c393122ea8f8dd9e2faf1e
Ava Gordon <avagordon01@gmail.com>
2023-08-02 18:44:29 +0100

    add note about mgba appending timestamp to save files

--- scripts/mgba-to-lemuroid.sh
+++ scripts/mgba-to-lemuroid.sh
@@ -5,7 +5,9 @@ set -eux
 srm="Pokemon - Emerald Version (USA, Europe).srm"
 sav="Pokemon - Emerald Version (USA, Europe).sav"
 
-# mgba appends 32 unknown bytes to the end of the 128KB save
+#mgba appends 32 bytes for RTC to the end of the 128KB save (lemuroid doesn't like this)
+#time-based events are a tiny part of emerald, so it might be fine to truncate this RTC footer
+#https://bulbapedia.bulbagarden.net/wiki/Time#Generation_III
 head -c 131072 "${sav}" > "${srm}"
 
 adb push "${srm}" "/storage/self/primary/Android/data/com.swordfish.lemuroid/files/saves"
